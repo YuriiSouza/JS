02c7b0d7f67ca9b633128477171f4145968a797a
Yuri <yuripeixoto1112@gmail.com>
2022-01-10 16:03:16 -0300

    estudos do dia

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,11 +1,13 @@
 var x;
 
-for (x = 1; x < 101; x++){
-    if(x%3=0){
-        console.log("Fizz");
-    }else if(x%5=0){
+for (x = 1; x <= 100; x++){
+    if (x%3 == 0 && x%5 == 0){
+        console.log("FizzBuzz");
+    }else if(x%5 == 0 ){
         console.log("Buzz");
-    }else { 
+    }else if(x%3 == 0 ){
+    console.log("Fizz");
+    }else{
         console.log(x);        
     }
 }
